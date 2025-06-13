557527842131188acf6b8304233ada1c3090c7c9
chakshit-002 <gunidiachakshit@gmail.com>
2025-06-13 21:30:03 +0530

    adding homepage responsive half code

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
+
+import React from 'react'
+import Navbar from './components/Navbar'
+
+
 import React from "react";
-import Navbar from "../src/components/Navbar";
+
 import Mainroutes from "./Routes/Mainroutes";
 
 
 const App = () => {
   return (
+
     <div className="bg-[#f4e7cf]">
+<<<<<<< HEAD
       {/* <Navbar /> */}
+=======
+      <Navbar />
+
+    <div>
+ 
+      <Navbar/>
+
+>>>>>>> fd665e27eebb1de118365aecc61395f8b856b99d
       <Mainroutes/>
     </div>
-  );
-};
+  )
+}
+
 
 export default App;
