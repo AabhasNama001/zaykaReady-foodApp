c0b6cdb81383094c3087240b86f9229ac9fe5e75
Aabhas Nama <aabhasa9@gmail.com>
2025-06-14 21:25:40 +0530

    Half home completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,14 @@
-
-import React from 'react'
-
-import Navbar from './components/Navbar.jsx'
+import React from "react";
+import Navbar from "./components/Navbar.jsx";
 import Mainroutes from "./Routes/Mainroutes.jsx";
-
-
-
-
 
 const App = () => {
   return (
-
-    <div className="bg-[#f4e7cf]">
-
-
+    <div className="bg-[#f4e7cf] xl:px-15">
       {/* <Navbar /> */}
-
-      
-
-    
-        <Mainroutes/>
-    
-
+      <Mainroutes />
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
